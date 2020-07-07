a9081142e09acd2e04566c032ad6a5137f39c89c
RotemCarmon <tetch6@gmail.com>
2020-07-08 01:49:01 +0300

    static serve

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('<h1>My Node App</h1>')
-});
+
+app.use(express.static(path.join(__dirname, '../public_html')));
+
+// app.get('/', (req, res) => {
+//   res.send('<h1>My Node App</h1>')
+// });
 
 app.listen(5000, () => {
   console.log('App is listening on port 5000!');
