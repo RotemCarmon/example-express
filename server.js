eb6d43588201eabc6277f6dfcfb005aa51795e50
Rotem Carmon <tetch6@gmail.com>
2022-01-15 15:55:49 +0200

    Changed response text again

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors());
 let connectionType;
 
 app.get('/', (req, res) => {
-  res.send('Hello Node JS updated!')
+  res.send('Hello Node JS updated again!!!')
 });
 
 app.post('api/auth/login', (req,res) => {
