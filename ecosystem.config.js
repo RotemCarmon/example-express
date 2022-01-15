c0300b4c4ea9f463a36ab14b1d5c9544e0afa446
Rotem Carmon <tetch6@gmail.com>
2022-01-15 15:33:36 +0200

    Changed ecosystem.config.js file path to absolute

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,7 +19,7 @@ module.exports = {
       host: '192.117.146.217',
       ref: 'origin/master',
       repo: 'git@github.com:RotemCarmon/example-express.git',
-      path: 'var/app/repos',
+      path: '/var/app/repos',
       "pre-setup": "rm -rf /var/app/repos",
       'pre-deploy-local': '',
       'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production && pm2 save'
