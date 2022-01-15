d2fed776a6e859fcfe277a6dbbde3081ad42c869
Rotem Carmon <tetch6@gmail.com>
2022-01-15 15:20:23 +0200

    added pm2 ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  apps: [{
+    script: 'node server.js',
+    watch: '.',
+    env: {
+      NODE_ENV: "development"
+    },
+    env_production: {
+      NODE_ENV: "production"
+    }
+  }, {
+    script: './service-worker/',
+    watch: ['./service-worker']
+  }],
+
+  deploy: {
+    production: {
+      user: 'root',
+      host: '192.117.146.217',
+      ref: 'origin/master',
+      repo: 'git@github.com:RotemCarmon/example-express.git',
+      path: 'var/app/repos',
+      'pre-deploy-local': '',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production && pm2 save',
+      'pre-setup': ''
+    }
+  }
+};
