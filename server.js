b951c09eb06f4c01ecb0db5c4a94072309919045
Rotem Carmon <tetch6@gmail.com>
2022-01-15 13:43:03 +0200

    added giitignore and changed server response

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
+const fs = require('fs');
 const express = require('express');
 const path = require('path');
 const app = express();
+const http = require('http').createServer(app);
+const session = require('express-session');
+const cors = require('cors')
+
+const sessionOptions = {
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}
+app.use(session(sessionOptions));
+app.use(cors());
+
+let connectionType;
+
+app.get('/', (req, res) => {
+  res.send('Hello Node JS')
+});
+
+app.post('api/auth/login', (req,res) => {
+  req.session.user = {
+    name: 'Puki',
+    age: 80
+  }
+  res.send(req.session)
+})
 
 
-app.use(express.static(path.join(__dirname, '../public_html')));
-
-// app.get('/', (req, res) => {
-//   res.send('<h1>My Node App</h1>')
-// });
-
-app.listen(5000, () => {
-  console.log('App is listening on port 5000!');
+const port = 3080
+http.listen(port, () => {
+  connectionType = 'HTTP'
+  console.log('App is listening on port ' + port);
 });
